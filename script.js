6b2d5346f147cd6a0d150bff92233c9b04fe26b8
zia <316544808+im-zaia@users.noreply.github.com>
2026-08-14 19:15:38 +1200

    render scene

--- script.js
+++ script.js
@@ -6,8 +6,13 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement )
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const material = new THREE.MeshBasicMaterial( { color: 0x00CC66 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
 camera.position.z = 5;
+
+function animate( time ) {
+  renderer.render( scene, camera );
+}
+renderer.setAnimationLoop( animate );
