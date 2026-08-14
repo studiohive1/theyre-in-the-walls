9c103b75451cd2cf6c22b3884a16d1d88cf43e78
zia <316544808+im-zaia@users.noreply.github.com>
2026-08-14 20:11:14 +1200

    animate cube

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
+renderer.setAnimationLoop( animate );
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement )
 
@@ -13,6 +14,10 @@ scene.add( cube );
 camera.position.z = 5;
 
 function animate( time ) {
+
+  cube.rotation.x = time / 2000;
+  cube.rotation.y = time / 1000;
+
   renderer.render( scene, camera );
+
 }
-renderer.setAnimationLoop( animate );
