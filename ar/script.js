aec1536605ed4dc997bb4607835067eb9de58f2a
zia <316544808+im-zaia@users.noreply.github.com>
2026-08-17 22:54:17 +1200

    add model loading and camera access

--- ar/script.js
+++ ar/script.js
@@ -1,1 +1,15 @@
-console.log( tmImage );
+tmImage.load( 'model/model.json', 'model/metadata.json' )
+  .then( model => {
+    console.log( 'loaded', model.getTotalClasses() );
+  })
+  .catch( err => {
+    console.log( 'error', err.message );
+  });
+
+navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } })
+  .then( stream => {
+    document.getElementById( 'cam' ).srcObject = stream;
+  })
+  .catch( err => {
+    console.log( 'camera error', err.name );
+  });
