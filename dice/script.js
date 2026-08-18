033d1b5f5ba618176fbb74f9ab51fc43d4c10376
zia <316544808+im-zaia@users.noreply.github.com>
2026-08-18 19:28:50 +1200

    adjust dice snapping animation

--- dice/script.js
+++ dice/script.js
@@ -112,14 +112,11 @@ function animate( time ) {
     roll_y *= 0.96;
     // 0.96 or 0.97 seems fine for now, might adjust later!
 
-    if ( Math.abs( roll_x ) + Math.abs( roll_y ) < 0.01 ) {
+    if ( Math.abs( roll_x ) + Math.abs( roll_y ) < 0.04 ) {
       // console.log( 'stopped' );
       const quarter = Math.PI / 2;
-      cube.rotation.x += ( Math.round( cube.rotation.x / quarter ) * quarter - cube.rotation.x ) * 0.3;
-      cube.rotation.y += ( Math.round( cube.rotation.y / quarter ) * quarter - cube.rotation.y ) * 0.3;
-      // around 0.2-0.5 seems alright, might ask the team what they think
-      roll_x = 0;
-      roll_y = 0;
+      cube.rotation.x += ( Math.round( cube.rotation.x / quarter ) * quarter - cube.rotation.x ) * 0.15;
+      cube.rotation.y += ( Math.round( cube.rotation.y / quarter ) * quarter - cube.rotation.y ) * 0.15;
       stopped = true;
     }
   }
