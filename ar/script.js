0608d6fc1b43221c8fcb519eb5d70e51df1acde5
zia <316544808+im-zaia@users.noreply.github.com>
2026-08-19 19:48:22 +1200

    add ar page buttons and sound effects

--- ar/script.js
+++ ar/script.js
@@ -53,3 +53,15 @@ setInterval( () => {
   });
 
 }, 2000 );
+
+const click_sound = new Audio( 'assets/sound-click.wav' );
+
+settings.addEventListener( 'click', () => {
+  click_sound.currentTime = 0;
+  click_sound.play();
+});
+
+back.addEventListener( 'click', () => {
+  click_sound.currentTime = 0;
+  click_sound.play();
+});
