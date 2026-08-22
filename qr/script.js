393182e43fb4d0d07c71591f54ab03cb99ee82a0
zia <316544808+im-zaia@users.noreply.github.com>
2026-08-22 20:23:12 +1200

    replace QR folder based on AR folder

--- qr/script.js
+++ qr/script.js
@@ -1,12 +1,45 @@
-const click_sound = new Audio( 'assets/sound-click.wav' );
+navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } })
+  .then( stream => {
+    cam.srcObject = stream;
+  })
+  .catch( err => {
+    guide.textContent = 'camera error: ' + err.name;
+  });
 
-pause.addEventListener( 'click', () => {
-  click_sound.currentTime = 0;
-  click_sound.play();
+const cam = document.getElementById( 'cam' );
+const guide = document.getElementById( 'guide' );
+
+const click_sound = new Audio( 'assets/sound-click.wav' );
+const button_sound = new Audio( 'assets/sound-button.wav' );
+
+function play( sound ) {
+  sound.currentTime = 0;
+  sound.play();
+}
+
+submit.addEventListener( 'click', () => {
+  play( button_sound );
 });
 
-back.addEventListener( 'click', () => {
-  click_sound.currentTime = 0;
-  click_sound.play();
-  location.href = '../';
+pause.addEventListener( 'click', () => {
+  play( click_sound );
+});
+
+hint.addEventListener( 'click', () => {
+  play( click_sound );
+});
+
+reroll.addEventListener( 'click', () => {
+  play( click_sound );
+  overlay.className = 'show';
+});
+
+popup_close.addEventListener( 'click', () => {
+  play( click_sound );
+  overlay.className = '';
+});
+
+popup_yes.addEventListener( 'click', () => {
+  play( button_sound );
+  location.href = '../dice/';
 });
