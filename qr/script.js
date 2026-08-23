c1d358ea8b7253acbdc7a2bbd7ee6871ee6a1dfc
zia <316544808+im-zaia@users.noreply.github.com>
2026-08-24 00:38:27 +1200

    add correct screen and bracket images to qr page

--- qr/script.js
+++ qr/script.js
@@ -11,6 +11,9 @@ const guide = document.getElementById( 'guide' );
 
 const click_sound = new Audio( 'assets/sound-click.wav' );
 
+let is_right = false;
+let done = false;
+
 function play( sound ) {
   sound.currentTime = 0;
   sound.play();
@@ -18,6 +21,17 @@ function play( sound ) {
 
 submit.addEventListener( 'click', () => {
   play( click_sound );
+  if ( done ) return;
+  if ( is_right ) {
+    done = true;
+    save_done();
+    document.body.classList.add( 'solved' );
+  }
+});
+
+next.addEventListener( 'click', () => {
+  play( click_sound );
+  location.href = '../dice/';
 });
 
 pause.addEventListener( 'click', () => {
@@ -43,5 +57,40 @@ popup_yes.addEventListener( 'click', () => {
   location.href = '../dice/';
 });
 
-fill_progress( 1 );
-// temporary
+const supa_api = 'https://jxsilhqrwbnytjghdwdw.supabase.co/';
+const supa_key = 'sb_publishable_xhJeQe0pPWiMq19Q5UgwgA_8b5mAJUg';
+const db = supabase.createClient( supa_api, supa_key );
+
+const test_user = 'd860b0b8-2eae-480e-b858-994873709af7';
+
+function save_done() {
+  db.from( 'progress' )
+    .insert({
+      user_id: test_user,
+      exhibit_id: 'telecomm',
+      challenge_id: 'qr-1',
+      completed: true,
+      completed_at: new Date()
+    })
+    .then( result => {
+      console.log( result );
+      count_progress();
+    });
+}
+
+function count_progress() {
+  db.from( 'progress' )
+    .select( 'challenge_id' )
+    .eq( 'user_id', test_user )
+    .eq( 'exhibit_id', 'telecomm' )
+    .eq( 'completed', true )
+    .then( result => {
+      if ( done ) {
+        fill_progress( result.data.length );
+      } else {
+        fill_progress( result.data.length, 'qr' );
+      }
+    });
+}
+
+count_progress();
